dda58c48931efc23d27fefd003de06b7e997e3d3
Miranda <140850122+Miranda-08@users.noreply.github.com>
2023-12-30 17:17:42 +0000

    Falta apenas media queries

--- RuiMiranda_2023137879_TiagoCabral_2018020685/logica.js
+++ RuiMiranda_2023137879_TiagoCabral_2018020685/logica.js
@@ -16,3 +16,131 @@ function checkScroll() {
 }
 
 window.addEventListener('scroll', checkScroll); // Invoca a função sempre que o scroll muda
+
+// Abrir/Fechar Div  Mensal *************************************************************************
+let unica = document.querySelector('#unica');
+let mensal = document.querySelector('#mensal');
+let mensalDiv = document.querySelector('.Mensal');
+let checkmensal = 0
+
+
+function Display(doacao, divX) {
+    if (doacao === 'Mensal'){
+        divX.style.display = 'block';
+    }
+    else{
+        divX.style.display = 'none'
+    }
+}
+
+unica.addEventListener('click', function() {
+   Display('unica', mensalDiv);
+   return checkmensal = 0;
+});
+mensal.addEventListener('click', function() {
+    Display('Mensal', mensalDiv);
+    return checkmensal = 1;
+ });
+
+ // Abrir/Fechar Divs  Pagamento Unico/Mensal *************************************************************************
+ let PagamentoUnicoDiv = document.querySelector('#PagamentoUnico');
+ let PagamentoMensalDiv = document.querySelector('#PagamentoMensal');
+
+ function DisplayDuplo(doacao, divX, divY) {
+    if (doacao === 'Mensal'){
+        divX.style.display = 'block';
+        divY.style.display = 'none';
+    }
+    else{
+        divX.style.display = 'none';
+        divY.style.display = 'block';
+    }
+}
+ 
+ unica.addEventListener('click', function() {
+    DisplayDuplo('unica', PagamentoMensalDiv, PagamentoUnicoDiv)
+ });
+ mensal.addEventListener('click', function() {
+     DisplayDuplo('Mensal', PagamentoMensalDiv, PagamentoUnicoDiv);
+  });
+
+// Abrir/Fechar Divs Particular/Empresa*************************************************************************
+let particular = document.querySelector('#particular');
+let empresa = document.querySelector('#empresa');
+let particularDiv = document.querySelector('.Particular');
+let empresaDiv = document.querySelector('.Empresa');
+
+function toggleFields(tipo) {
+    if (tipo === 'Particular') {
+        particularDiv.style.display = 'block';
+        empresaDiv.style.display = 'none';
+    }
+    else {
+        particularDiv.style.display = 'none';
+        empresaDiv.style.display = 'block';
+    }
+}
+
+particular.addEventListener('click', function() {
+    toggleFields('Particular');
+}); 
+empresa.addEventListener('click', function() {
+    toggleFields('Empresa');
+});
+
+// Botoes preenchem input valor*************************************************************************
+let valorInput = document.querySelector('#valor');
+let botoes_blocoB = document.querySelectorAll('.BlocoBotoes button'); //buscar todos os elementos que correspondem ao seletor CSS especificado ('.BlocoB button')
+//cria uma NodeList (uma lista de nós) que contém todos os botões dentro dos elementos com a classe .BlocoB
+  
+botoes_blocoB.forEach(function(botao) { //Para cada elemento da NodeList (elementos a que chamaremos de botao) faz:
+    botao.addEventListener('click', function(event) {
+        event.preventDefault(); // Para evitar a navegação ao clicar no link dentro do botão
+        var valorbotao = parseInt(botao.id); // Obtém o valor do ID do botão em questão como um número
+        valorInput.value = valorbotao;
+    });
+});
+
+// Número de Refeições*************************************************************************
+
+var form = document.querySelector('form');
+var resultadoDiv = document.getElementById('resultado'); // Elemento para exibir a mensagem
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário para evitar recarregar a página
+
+    var valorDoacao = parseFloat(valorInput.value); // Converte o valor do input para um número real
+
+    const custoRefeicao = 1.5;
+    var numeroRefeicoes = Math.floor(valorDoacao / custoRefeicao); // número de refeições que a doação permite comprar
+
+    // Cria uma mensagem com o número de refeições
+
+    if (checkmensal==0){
+        if (valorDoacao > 40) {
+            var numeroPessoas = Math.floor(numeroRefeicoes / 2); // Duas refeições por dia
+            var mensagem = "O seu donativo permitirá alimentar diariamente aproximadamente " + numeroPessoas + " pessoas";
+        } else {
+            var mensagem = "O seu donativo permitirá fornecer aproximadamente " + numeroRefeicoes + " refeições";
+        }
+    }
+
+    else{
+        if (valorDoacao < 90) {
+            var numeroDias = Math.floor(numeroRefeicoes / 2);
+            var mensagem = "O seu donativo permitirá alimentar uma pessoa durante " + numeroDias + " dias";
+        }else{
+            var numeroPessoasMensal = Math.floor(numeroRefeicoes / 60)
+            var mensagem = "O seu donativo permitirá alimentar " + numeroPessoasMensal + " pessoas durante este mês";
+        }
+    }
+
+    // Exibe a mensagem no console (substitua isso pela sua lógica de exibição real)
+    resultadoDiv.style.display = 'block';
+    resultadoDiv.textContent = mensagem;
+});
+  
+  
+  
+
+  
